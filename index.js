6319d9fd06cb244d28593797123ee02c0e523779
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-08 06:17:17 +0000

    User age stuff

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 console.log("Hey...");
 var readlinesync = require('readline-sync');
-var userName = readlinesync.question("Please enter your name ");
-console.log("Welcome " + userName.toUpperCase() + "!");
+var score = 0;
+var userAge = readlinesync.question("Am I older than 25? Yes/No? ");
+if(userAge === "Yes"){
+  console.log("You are right");
+   score = score+1;
+}
+else{
+  console.log("You are Wrong");
+  score = score-1;
+}
+console.log("Your score is " + score);
