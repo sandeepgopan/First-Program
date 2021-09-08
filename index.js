89610999b49a87037f8ecfab8907469695e4f0c7
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-08 06:19:22 +0000

    Till age check

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ console.log("Hey...");
 var readlinesync = require('readline-sync');
 var userName = readlinesync.question("Please enter your name ");
 console.log("Welcome " + userName.toUpperCase() + "!");
+var userAge = readlinesync.question("Am I older than 25? Yes/No? ");
+var score = 0;
+if(userAge === "Yes"){
+  console.log("You are right");
+   score = score+1;
+}
+else{
+  console.log("You are Wrong");
+  score = score-1;
+}
+console.log("Your score is " + score);
