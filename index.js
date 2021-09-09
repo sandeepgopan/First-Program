92061eef24489fc0d243f8decd8c15dccde44af3
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-09 15:34:05 +0000

    Function stars

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-console.log("Hey...");
-var readlinesync = require('readline-sync');
-var userName = readlinesync.question("Please enter your name ");
-console.log("Welcome " + userName.toUpperCase() + "!");
-var userAge = readlinesync.question("Am I older than 25? Yes/No? ");
-var score = 0;
-if(userAge === "Yes"){
-  console.log("You are right");
-   score = score+1;
+var readlineSync = require('readline-sync');
+let n = readlineSync.question("enter a number ");
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string = string+ "*";
+  }
+string = string+  "\n";
 }
-else{
-  console.log("You are Wrong");
-  score = score-1;
+console.log(string);
+
+var readlineSync = require('readline-sync');
+let l = readlineSync.question("enter a number ");
+let stringl = "";
+for (let i = 0; i <= l; i++) {
+  for (let j = 0; j < l; j++) {
+    stringl = stringl + "*";
+  }
+stringl = stringl +  "\n";
 }
-console.log("Your score is " + score);
+console.log(stringl);
+
+var readlineSync = require('readline-sync');
+let l1 = readlineSync.question("enter a number ");
+let stringl1 = "";
+for (let i = 0; i <= l1; i++) {
+  for (let j = 0; j < (l1 - i); j++) {
+    stringl1 = stringl1 + "*";
+  }
+stringl1 = stringl1 +  "\n";
+}
+console.log(stringl1);
