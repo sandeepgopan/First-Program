8dbf01e8355ae63c93e00fff3ec1390fd4943929
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-09 15:42:31 +0000

    array

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-console.log("Hey...");
-var readlinesync = require('readline-sync');
-var userName = readlinesync.question("Please enter your name ");
-console.log("Welcome " + userName.toUpperCase() + "!");
-var userAge = readlinesync.question("Am I older than 25? Yes/No? ");
-var score = 0;
-if(userAge === "Yes"){
-  console.log("You are right");
-   score = score+1;
+var groceryList = ['rava','idle','dosa','uppuma','porotta','chappathi'];
+for (var i= 0 ; i< groceryList.length ; i++){
+  console.log(groceryList[i]);
 }
-else{
-  console.log("You are Wrong");
-  score = score-1;
-}
-console.log("Your score is " + score);
