b34271e6fcdfffec0edc6acb505a2269cb0d4140
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-09 16:17:42 +0000

    object

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var groceryList = ['rava','idle','dosa','uppuma','porotta','chappathi'];
-for (var i= 0 ; i< groceryList.length ; i++){
-  console.log(groceryList[i]);
+var superMan = {
+  strenght : "1000",
+  stealth : "10",
+  power : "lighter"
+}
+var batMan = {
+  strenght : "22000",
+  stealth : "10",
+  power : "lighter"
+}
+console.log(superMan.strenght);
+if(superMan.strenght > batMan.strenght){
+  console.log("Superman is Stronger");
+}
+else{
+  console.log("batMan");
 }
